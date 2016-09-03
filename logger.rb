df44342ca6b971b386c5dd8fb8e09f04a2972f89
“reddrum” <anton.kranz@me.com>
2016-09-03 16:15:41 +0300

    Update logger

--- logger.rb
+++ logger.rb
@@ -1,18 +1,13 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open 'log.txt', 'a'
   end
 
-  @@x = Logger.new
-
-  def self.instance
-    return @@x
-  end
-
-  # instance method
   def log_something wat
     @f.puts wat
   end
-
-  private_class_method :new
 end
